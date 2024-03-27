505cdbc3d4e46e4f481db170ebc36cce6ce717df
Luiz Passos <luizsilva.miui@gmail.com>
2024-03-27 01:15:24 -0300

    first commit

---
+++ markers/admin.py
@@ -0,0 +1,9 @@
+from django.contrib.gis import admin
+from leaflet.admin import LeafletGeoAdmin
+from markers.models import Marker
+
+
+@admin.register(Marker)
+class MarkerAdmin(LeafletGeoAdmin):
+    list_display = ("name", "description", "location", "hyperlink")
+    search_fields = ("name", "description")
