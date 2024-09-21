90b2b8021e32f77bcee01a2137beac275b7e63f9
Luiz Passos <luizsilva.miui@gmail.com>
2024-09-21 17:32:33 -0300

    to update

--- markers/admin.py
+++ markers/admin.py
@@ -9,6 +9,25 @@ class DenunciaAdmin(admin.ModelAdmin):
     list_display = ("nome_completo", "motivo", "cidade", "data_envio")  # Campos mostrados na lista
     search_fields = ("nome_completo", "motivo", "cidade")  # Campos disponíveis para pesquisa
 
+    # Impedir adição de novos registros
+    def has_add_permission(self, request):
+        return False
+
+    # Impedir a edição de registros
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    # Permitir exclusão de registros
+    def has_delete_permission(self, request, obj=None):
+        return True
+
+    # Tornar todos os campos somente leitura
+    def get_readonly_fields(self, request, obj=None):
+        if obj:  # Para um objeto existente, todos os campos serão somente leitura
+            return [f.name for f in obj._meta.fields]
+        return super().get_readonly_fields(request, obj)
+
+
 # Configuração da interface de administração para o modelo Marker usando LeafletGeoAdmin
 @admin.register(Marker)
 class MarkerAdmin(LeafletGeoAdmin):
